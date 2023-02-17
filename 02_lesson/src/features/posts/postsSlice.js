c6004f4f32000c33e293fc50e75576de92d8a239
benjdg16dev <benjdg16dev@gmail.com>
2023-02-17 17:44:55 -0500

    [feat] 02_lesson

---
+++ 02_lesson/src/features/posts/postsSlice.js
@@ -0,0 +1,81 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: `I've heard good things.`,
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: `The more I say slice, the more I want pizza.`,
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        // Note: "push" mutates the state
+        // but redux-toolkit uses ImmerJS
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        // Note: This will mutate the state.
+        // But since we are inside createSlice it will be handled
+        // by ImmerJS
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+});
+
+// useSelector callback
+export const selectAllPosts = (state) => state.posts;
+
+// Note: Automatically creates actions
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
